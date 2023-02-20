47aa485a69124a4f3420228aede9357364925282
Artjoms Spole <artjoms.spole@gmail.com>
2023-02-20 09:25:39 +0200

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Artjoms Spole on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
